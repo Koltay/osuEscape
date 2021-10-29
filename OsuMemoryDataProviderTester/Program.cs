b8a9d017f8d6740bf53ac462010747f32d87dfb1
Koltay <yatlok1017@gmail.com>
2021-10-30 07:11:50 +0800

    osu data display update
    copied the whole file into the project,
    modified the only data needed for the functions on form.
    
    Show function not working tho

---
+++ OsuMemoryDataProviderTester/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace OsuMemoryDataProviderTester
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new OsuMemoryDataProviderForm(args.FirstOrDefault()));
+        }
+    }
+}
